a0976c207e9672b434523b960873d4aa090dcc97
Hubert-Pastyrzak <97910195+Hubert-Pastyrzak@users.noreply.github.com>
2022-04-19 14:20:15 +0200

    Implement a search bar

---
+++ search.js
@@ -0,0 +1,36 @@
+class search {
+    static setDictionary(dictionary) {
+        search.dictionary = dictionary;
+
+        for (key in dictionary) {
+            let link = document.createElement("a");
+            link.setAttribute("href", key);
+            link.appendChild(document.createTextNode(dictionary[key]));
+
+            let paragraph = document.createElement("p");
+            paragraph.appendChild(link);
+
+            document.getElementById("search.results").appendChild(paragraph);
+        }
+    }
+};
+
+document.getElementById("search.text").addEventListener("input", function(e) {
+    let results = document.getElementById("search.results");
+
+    while (results.firstChild)
+        results.removeChild(results.lastChild);
+
+    for (key in search.dictionary) {
+        if (search.dictionary[key].includes(e.target.value)) {
+            let link = document.createElement("a");
+            link.setAttribute("href", key);
+            link.appendChild(document.createTextNode(search.dictionary[key]));
+
+            let paragraph = document.createElement("p");
+            paragraph.appendChild(link);
+
+            results.appendChild(paragraph);
+        }
+    }
+});
