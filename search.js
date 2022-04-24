e2ed416e8af935a31ddd537a23848ad3e0f9a880
Hubert-Pastyrzak <97910195+Hubert-Pastyrzak@users.noreply.github.com>
2022-04-24 19:52:19 +0200

    Load the dictionary from an external file

--- search.js
+++ search.js
@@ -1,32 +1,34 @@
-let dictionary = {
-    //TODO: Load the dictionary
-};
+let dictionary;
 
-let params = new URLSearchParams(document.location.search);
-let query = params.get("query");
+fetch("/dictionary.json")
+    .then(response => response.json())
+    .then(json => {
+        let params = new URLSearchParams(document.location.search);
+        let query = params.get("query");
 
-document.title = `Wyniki wyszukiwania dla „${query}” - Język angielski`;
-document.getElementById("query").appendChild(document.createTextNode(query));
+        document.title = `Wyniki wyszukiwania dla „${query}” - Język angielski`;
+        document.getElementById("query").appendChild(document.createTextNode(query));
 
-let found = false;
+        let found = false;
 
-for (let [key, value] of Object.entries(dictionary)) {
-    if (value.includes(query)) {
-        let link = document.createElement("a");
-        link.setAttribute("href", key);
-        link.appendChild(document.createTextNode(value));
+        for (let [key, value] of Object.entries(dictionary)) {
+            if (value.includes(query)) {
+                let link = document.createElement("a");
+                link.setAttribute("href", key);
+                link.appendChild(document.createTextNode(value));
 
-        let paragraph = document.createElement("p");
-        paragraph.appendChild(link);
+                let paragraph = document.createElement("p");
+                paragraph.appendChild(link);
 
-        document.getElementById("results").appendChild(paragraph);
-        found = true;
-    }
-}
+                document.getElementById("results").appendChild(paragraph);
+                found = true;
+            }
+        }
 
-if (!found) {
-    let paragraph = document.createElement("p");
-    paragraph.appendChild(document.createTextNode("Nie znaleziono niczego."));
+        if (!found) {
+            let paragraph = document.createElement("p");
+            paragraph.appendChild(document.createTextNode("Nie znaleziono niczego."));
 
-    document.getElementById("results").appendChild(paragraph);
-}
+            document.getElementById("results").appendChild(paragraph);
+        }
+    });
