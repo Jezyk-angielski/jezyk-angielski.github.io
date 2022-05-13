92f8e9acd65944bfc9237f422baedd2deb65ad30
Hubert Pastyrzak <97910195+Hubert-Pastyrzak@users.noreply.github.com>
2022-05-13 10:13:52 +0200

    Add support for searching both lessons and words

--- search.js
+++ search.js
@@ -7,30 +7,60 @@ document.getElementById("query-link").href = document.location.search;
 for (let queryElement of document.getElementsByClassName("query"))
     queryElement.appendChild(document.createTextNode(query));
 
-fetch("/dictionary.json")
-    .then(response => response.json())
-    .then(dictionary => {
-        let found = false;
+fetch("/course.json")
+    .then(course_response => course_response.json())
+    .then(course => {
+        let lessons_found = false;
 
         for (let [key, value] of Object.entries(dictionary)) {
             if (value.toLowerCase().includes(query.toLowerCase())) {
                 let link = document.createElement("a");
-                link.setAttribute("href", `/slownik/${key}`);
+                link.setAttribute("href", `/kurs/${key}`);
                 link.appendChild(document.createTextNode(value));
 
                 let paragraph = document.createElement("p");
                 paragraph.appendChild(link);
 
-                document.getElementById("results").appendChild(paragraph);
-                found = true;
+                document.getElementById("results-lessons").appendChild(paragraph);
+                lessons_found = true;
             }
         }
 
-        document.getElementById("loading").style.display = "none";
-        if (!found) {
+        if (!lessons_found) {
             let paragraph = document.createElement("p");
             paragraph.appendChild(document.createTextNode("Nie znaleziono niczego."));
 
-            document.getElementById("results").appendChild(paragraph);
+            document.getElementById("results-lessons").appendChild(paragraph);
         }
+
+        fetch("/dictionary.json")
+            .then(dictionary_response => dictionary_response.json())
+            .then(dictionary => {
+                let words_found = false;
+
+                for (let [key, value] of Object.entries(dictionary)) {
+                    if (value.toLowerCase().includes(query.toLowerCase())) {
+                        let link = document.createElement("a");
+                        link.setAttribute("href", `/slownik/${key}`);
+                        link.appendChild(document.createTextNode(value));
+
+                        let paragraph = document.createElement("p");
+                        paragraph.appendChild(link);
+
+                        document.getElementById("results-words").appendChild(paragraph);
+                        words_found = true;
+                    }
+                }
+
+                if (!words_found) {
+                    let paragraph = document.createElement("p");
+                    paragraph.appendChild(document.createTextNode("Nie znaleziono niczego."));
+
+                    document.getElementById("results-words").appendChild(paragraph);
+                }
+
+                document.getElementById("loading").style.display = "none";
+                document.getElementById("results-lessons").style.display = "block";
+                document.getElementById("results-words").style.display = "block";
+            });
     });
