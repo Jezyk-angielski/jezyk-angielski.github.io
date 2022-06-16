c5fe708dd7bad158cae629450c9e96cce164292b
Hubert-Pastyrzak <97910195+Hubert-Pastyrzak@users.noreply.github.com>
2022-06-16 22:21:47 +0200

    Add a back arrow

--- lesson.js
+++ lesson.js
@@ -1,6 +1,29 @@
 let pages = document.getElementsByClassName("page").length;
 let current_page = 1;
 
+document.getElementById("arrow").addEventListener("click", e => {
+    if (e.target.className.includes("grayed"))
+        return;
+
+    document.getElementById("progress-bar").value--;
+
+    if (current_page == 1) {
+        window.location.href = "/kurs";
+        return;
+    }
+
+    document.getElementById("arrow").className = "grayed";
+    document.getElementById("next").className = "grayed";
+    document.getElementById("footer").style.display = "none";
+
+    for (let footer of document.getElementsByTagName("footer")) {
+        if (footer.className.includes(`page-${current_page}`))
+            footer.style.display = "none";
+    }
+
+    document.getElementById(`page-${current_page}`).style.animation = "page_exit_backwards_animation 0.5s";
+});
+
 document.getElementById("next").addEventListener("click", e => {
     if (e.target.className.includes("grayed"))
         return;
@@ -12,6 +35,7 @@ document.getElementById("next").addEventListener("click", e => {
         return;
     }
 
+    document.getElementById("arrow").className = "grayed";
     document.getElementById("next").className = "grayed";
     document.getElementById("footer").style.display = "none";
 
@@ -25,9 +49,49 @@ document.getElementById("next").addEventListener("click", e => {
 
 for (let page of document.getElementsByClassName("page")) {
     page.addEventListener("animationend", e => {
-        if (e.target.style.animation.includes("page_enter_animation")) {
-            if (!document.getElementById(`page-${current_page}`).className.includes("exercise"))
+        if (e.target.style.animation.includes("page_enter_backwards_animation") || e.target.style.animation.includes("page_enter_animation")) {
+            document.getElementById("arrow").className = "";
+
+            if (document.getElementById(`page-${current_page}`).className.includes("exercise")) {    
+                for (let button of document.getElementsByTagName("button")) {
+                    if (button.className.includes(`page-${current_page}`) && button.className.includes("chosen")) {
+                        document.getElementById("next").className = "";
+
+                        if (button.className.includes("incorrect")) {
+                            document.getElementById("footer").style.display = "block";
+                            for (let footer of document.getElementsByTagName("footer")) {
+                                if (footer.className.includes(`page-${current_page} incorrect`))
+                                    footer.style.display = "block";
+                            }
+                        } else {
+                            document.getElementById("footer").style.display = "block";
+                            for (let footer of document.getElementsByTagName("footer")) {
+                                if (footer.className.includes(`page-${current_page} correct`)) {
+                                    console.log(footer.className);
+                                    footer.style.display = "block";
+                                }
+                            }
+                        }
+
+                        break;
+                    }
+                }
+            } else
                 document.getElementById("next").className = "";
+        }
+
+        if (e.target.style.animation.includes("page_exit_backwards_animation")) {
+            e.target.style.display = "none";
+
+            current_page--;
+
+            document.getElementById(`page-${current_page}`).style.display = "block";
+            document.getElementById(`page-${current_page}`).style.animation = "page_enter_backwards_animation 0.5s";
+
+            if (current_page < pages)
+                document.getElementById("next").textContent = "Dalej";
+
+            return;
         }
 
         if (e.target.style.animation.includes("page_exit_animation")) {
@@ -65,6 +129,9 @@ for (let correctButton of document.getElementsByClassName("correct")) {
         let page = correctButton.className.split(" ")[0];
 
         for (let button of document.getElementsByClassName(page)) {
+            if (button.tagName != "BUTTON")
+                continue;
+            
             if (button == e.target)
                 button.className = `${page} correct chosen`;
             else
@@ -92,6 +159,9 @@ for (let incorrectButton of document.getElementsByClassName("incorrect")) {
         let page = incorrectButton.className.split(" ")[0];
 
         for (let button of document.getElementsByClassName(page)) {
+            if (button.tagName != "BUTTON")
+                continue;
+
             if (button == e.target)
                 button.className = `${page} incorrect chosen`;
             else
